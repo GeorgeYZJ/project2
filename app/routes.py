f831537effe0a661199e98acc5a8a1f725c2c558
gnyirenda <nyirendahgn@gmail.com>
2020-05-19 16:00:23 +1000

    Added page routing

--- app/routes.py
+++ app/routes.py
@@ -12,12 +12,13 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect('/Quiz')
+
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/index')
 def index():
-    
+
     user = {'username': 'Godfrey Nyirenda'}
     posts = [
         {
@@ -25,7 +26,7 @@ def index():
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Susan'},
+            'author': {'username': 'Susan'}
             'body': 'The Avengers movie was so cool!'
         }
     ]
